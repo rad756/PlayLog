aca3f185c0905532ca055b4aaaeb8657e7077ba8
rad756 <radek9616@gmail.com>
2023-09-10 16:13:41 +0100

    Code cleanup

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 )
 
+// variables
+var splitOffset = 0.6
+var a = app.New()
+var mainWin = a.NewWindow("PlayLog")
+
 func main() {
-	ui()
+	mainWin.Resize(fyne.NewSize(600, 400))
+	icon, _ := fyne.LoadResourceFromPath("Icon.png")
+
+	gameTab := makeGameTab()
+	movieTab := makeMovieTab()
+	showTab := makeShowTab()
+
+	mainTab := container.NewAppTabs(
+		container.NewTabItem("Games", gameTab),
+		container.NewTabItem("Movies", movieTab),
+		container.NewTabItem("Shows", showTab))
+
+	mainWin.SetContent(mainTab)
+	mainWin.SetIcon(icon)
+	mainWin.ShowAndRun()
 }
 
 // checks if string contains a comma
 func noComma(s string) bool {
-	res := strings.Contains(s, ",")
-
-	if res {
-		return false
-	} else {
-		return true
-	}
+	return !strings.Contains(s, ",")
 }
